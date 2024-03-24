a4f35dbe6e43927f5ed2e577e6340f8b79ef6a0d
Sakerick <herickvizc@outlook.com>
2024-03-23 22:46:29 -0700

    Movimiento de clases a main/java
    movi las clases a su carpeta correspondiente

---
+++ app/src/main/java/ManoDeCartas.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class ManoDeCartas {
+    private ArrayList <Carta> Mano;
+    public ManoDeCartas(){
+        Mano=new ArrayList<Carta>();
+    }
+    public void agregarCartaAMano(int Posicion, Carta laCarta){
+        Mano.add(0,laCarta);
+    }
+
+    public String toString() {
+        String ManoCadena = "";
+
+        for (Carta carta : Mano) {
+            ManoCadena = ManoCadena + carta;
+        }
+        return ManoCadena;
+    }
+    public int getSizeDeMano(){
+        int sizeDeMano=Mano.size();
+        return sizeDeMano;
+    }
+    public Carta getCartaDeMano(int posicionDeCarta){
+        return Mano.get(posicionDeCarta);
+    }
+}
