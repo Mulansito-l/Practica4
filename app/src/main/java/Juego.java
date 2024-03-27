3c03bf28583c2dbdffcb4e424c448d37046d7cc4
Diego Castañeda Covarrubias <diego.castaneda35@uabc.edu.mx>
2024-03-27 16:13:00 -0700

    Combinar mains
    Juntando el main de la clase Main al main de la clase Juego.

--- app/src/main/java/Juego.java
+++ app/src/main/java/Juego.java
@@ -1,7 +1,9 @@
-
+    
 public class Juego{
     public static void main(String[] args){
         Sprite card = new Sprite("carta1.png",300, 300);
         Canvas.getCanvas().Draw(card, card);
+        elCinquillo game=new elCinquillo();
+        game.jugarElCinquillo();
     }
 }
--- app/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main{
-
-    public static void main(String[] args) {
-        elCinquillo game=new elCinquillo();
-        game.jugarElCinquillo();
-    }
-}
