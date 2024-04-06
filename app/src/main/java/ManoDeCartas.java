81af6efaa69bfca790b257ce56a5bfc4e9c5409a
Diego Castañeda Covarrubias <113644589+Mulansito-l@users.noreply.github.com>
2024-04-05 19:31:52 -0700

    Revert "Implementación de los métodos para colocar una carta"

--- app/src/main/java/ManoDeCartas.java
+++ app/src/main/java/ManoDeCartas.java
@@ -1,41 +1,31 @@
 import java.util.ArrayList;
 
 public class ManoDeCartas {
-    private ArrayList <Carta> mano;
+    private ArrayList <Carta> Mano;
     public ManoDeCartas(){
-        mano =new ArrayList<Carta>();
+        Mano=new ArrayList<Carta>();
     }
     public void agregarCartaAMano(int Posicion, Carta laCarta){
-        mano.add(0,laCarta);
+        Mano.add(0,laCarta);
     }
 
     public String toString() {
         String ManoCadena = "";
 
-        for (Carta carta : mano) {
+        for (Carta carta : Mano) {
             ManoCadena = ManoCadena + carta;
         }
         return ManoCadena;
     }
     public int getSizeDeMano(){
-        int sizeDeMano= mano.size();
+        int sizeDeMano=Mano.size();
         return sizeDeMano;
     }
-
-    public Carta getCartaDe(int valor, String palo){
-        for (Carta cartita:mano){
-            if (cartita.getValor() == valor && cartita.getPalo().equals(palo) ){
-                return cartita;
-            }
-        }
-        return null;
-    }
-
     public Carta getCartaDeMano(int posicionDeCarta){
-        return mano.get(posicionDeCarta);
+        return Mano.get(posicionDeCarta);
     }
     public void removerCartaDeMano(int posicionDeCarta) {
-        mano.remove(posicionDeCarta);
+        Mano.remove(posicionDeCarta);
 
     }
 }
--- app/src/main/java/Player.java
+++ app/src/main/java/Player.java
@@ -3,33 +3,24 @@ public class Player {
     private ManoDeCartas manoDelJugador;
     private int ladoElegido;
     Scanner scanner=new Scanner(System.in);
-    private boolean enTurno;
-    private int turno;
+    private boolean EnTurno;
+    private int Turno;
     private int puntuacionTotal;
     private int puntuacionDeRonda=0;
     private String estado="noWinner";
-    public Player(int turno) {
-        enTurno = true;
+    public Player() {
+        EnTurno = true;
         estado = "noWinner";
-        this.turno = turno;
         manoDelJugador=new ManoDeCartas();
-    }
-
-    public int getTurno(){
-        return turno;
     }
     public ManoDeCartas getManoDelJugador(){
         return manoDelJugador;
     }
-    public Carta getCartaDe(int valor, String palo){
-        return manoDelJugador.getCartaDe(valor, palo);
-    }
-
     public void AgregarAMano(Carta laCarta){
         manoDelJugador.agregarCartaAMano(0,laCarta);
     }
     public void setTurno(int Turno){
-        this.turno =Turno;
+        this.Turno=Turno;
     }
     public void mostrarMano(){
         for (int i=0;i<manoDelJugador.getSizeDeMano();i++) {
